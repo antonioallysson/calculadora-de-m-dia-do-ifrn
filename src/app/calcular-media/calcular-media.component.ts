502533c3df1eb3994521554fec31f018ecfc1ad8
antonioallysson <antonioallysson2020@gmail.com>
2024-09-07 17:33:07 -0300

    correção do limite de nota

--- src/app/calcular-media/calcular-media.component.ts
+++ src/app/calcular-media/calcular-media.component.ts
@@ -13,17 +13,26 @@ export class CalcularMediaComponent {
   boletim: Boletim | undefined;
 
   calcularMediaParcial(b1: number, b2: number, b3: number, b4: number) {
-    // Passando os valores dos bimestres para a classe Boletim
-    this.boletim = new Boletim(b1, b2, b3, b4);
-    this.mediaParcial = this.boletim.getMediaParcial();
-    this.situacao = this.boletim.getSituacao();
+    if (this.validaNota(b1) && this.validaNota(b2) && this.validaNota(b3) && this.validaNota(b4)) {
+      this.boletim = new Boletim(b1, b2, b3, b4);
+      this.mediaParcial = this.boletim.getMediaParcial();
+      this.situacao = this.boletim.getSituacao();
+    } else {
+      alert("Todas as notas devem estar entre 0 e 100.");
+    }
   }
 
   calcularMediaFinal(naf: number) {
-    if (this.boletim) {
+    if (this.boletim && this.validaNota(naf)) {
       this.boletim.registrarNotaFinal(naf);
       this.mediaFinal = this.boletim.getMediaFinal();
       this.situacao = this.boletim.getSituacao();
+    } else {
+      alert("A nota da avaliação final deve estar entre 0 e 100.");
     }
+  }
+
+  private validaNota(nota: number): boolean {
+    return nota >= 0 && nota <= 100;
   }
 }
